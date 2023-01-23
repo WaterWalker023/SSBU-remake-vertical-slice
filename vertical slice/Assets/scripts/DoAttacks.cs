f035a5930d6b15add58023d5779697eb5845b5f3
WaterWalker023 <32755@ma-web.nl>
2023-01-23 12:53:49 +0100

    platformen

---
+++ vertical slice/Assets/scripts/DoAttacks.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class DoAttacks : MonoBehaviour
+{
+    private GameObject HitboxInstance;
+    [SerializeField] private Attack Rollout;
+    [SerializeField] private Attack UpSmash;
+    [SerializeField] private Attack Jab;
+    [SerializeField] private Attack Jab2;
+    [SerializeField] private Attack attack;
+    private float jab2Cooldown = 2f;
+    private float UpsmashChargeCounter;
+    private float RolloutChargeCounter;
+    private float timer;
+    private float RolloutChargeTimer;
+    private float UpSmashChargeTimer;
+    public Rigidbody rb;
+    private float RolloutForce = 1;
+    public Jigglypuff jigglypuffScript;
+    public Transform ME;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ExecuteAttack(Jab);
+        }
+
+        if (Input.GetKey(KeyCode.O))
+        {
+            RolloutChargeCounter += Time.deltaTime;
+            RolloutChargeTimer += Time.deltaTime;
+        }
+
+        if (Input.GetKeyUp(KeyCode.O))
+        {
+            RolloutChargeTimer = 0;
+            Rollout.Damage += (RolloutChargeCounter / 60);
+            RolloutForce += RolloutChargeCounter * 60;
+            RolloutChargeCounter = 0;
+            ExecuteAttack(Rollout);
+            rb.AddForce(RolloutForce, 0, 0);
+        }
+
+        if (RolloutChargeTimer >= 3.7)
+        {
+            RolloutForce += RolloutChargeCounter * 60;
+            RolloutChargeTimer = 0;
+            RolloutChargeCounter = 0;
+            Rollout.Damage = 25.2f;
+            ExecuteAttack(Rollout);
+            rb.AddForce(RolloutForce, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            UpsmashChargeCounter += Time.deltaTime;
+            UpSmashChargeTimer += Time.deltaTime;
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            UpSmashChargeTimer = 0;
+            UpSmash.Damage = 15;
+            UpSmash.Damage += UpsmashChargeCounter / 60;
+            UpsmashChargeCounter = 0;
+            ExecuteAttack(UpSmash);
+        }
+
+        if (UpSmashChargeTimer >= 3.7)
+        {
+            UpSmashChargeTimer = 0;
+            UpsmashChargeCounter = 0;
+            UpSmash.Damage = 25.2f;
+            ExecuteAttack(UpSmash);
+        }
+
+        if (attack != null)
+        {
+            timer += Time.deltaTime;
+            
+            if (timer > attack.SpawnDelay && timer < attack.DespawnDelay)
+            {
+                if (HitboxInstance == null)
+                {
+                    HitboxInstance = Instantiate(attack.Hitbox,transform.position, Quaternion.identity);
+                    HitboxInstance.transform.parent = this.transform;
+                }
+                
+                if (attack.name == "rollout")
+                {
+                    Physics.IgnoreCollision(transform.GetComponent<Collider>(), HitboxInstance.GetComponent<Collider>(), true);
+                    HitboxInstance.transform.position = rb.transform.position;
+                }
+                else
+                {
+                    Physics.IgnoreCollision(transform.GetComponent<Collider>(), HitboxInstance.GetComponent<Collider>(), true);
+                    HitboxInstance.transform.position = (rb.transform.position + new Vector3(1f, 0.5f, 0));
+                }
+            }
+
+            if (attack.name == "Jab")
+            {
+                timer += Time.deltaTime;
+                if (timer < jab2Cooldown && (Input.GetKeyDown(KeyCode.P)))
+                {
+                    ExecuteAttack(Jab2);
+                }
+            }
+            if (timer > attack.DespawnDelay)
+            {
+                Destroy(HitboxInstance);
+                attack = null;
+                timer = 0;
+                if(attack.name == "upsmash")
+                {
+                    attack.Damage = 15;
+                }
+                if (attack.name == "rollout")
+                {
+                    attack.Damage = 10;
+                }
+            }
+
+        }
+    }
+
+    public void ExecuteAttack(Attack attack)
+    {
+        this.attack = attack;
+        Debug.Log(attack.name);
+        //animatie doen
+        
+    }
+
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Jigglypuff")
+        {
+            jigglypuffScript.damage = attack.Damage;
+            jigglypuffScript.Scaling = attack.Scaling;
+            jigglypuffScript.BaseKnockBack = attack.BaseKnockBack;
+            jigglypuffScript.AngleAttack = attack.AngleAttack;
+            jigglypuffScript.attacked = true;
+            Debug.Log("Aan raak");
+            rb.velocity = Vector3.zero;
+
+        }
+    }
+    
+}
--- vertical slice/Assets/scripts/Jigglypuff.cs
+++ vertical slice/Assets/scripts/Jigglypuff.cs
@@ -8,17 +8,17 @@ public class Jigglypuff : MonoBehaviour
     public bool attacked;
     public float percentage; //     de percentage van gameobject counted after the attack's damage is added
     public float damage; //         damage van last attack 
-    public float weight; //         de weight van gameobject
-    public float scaling; //        the attack's knockback scaling (also known as knockback growth) divided by 100 (so a scaling of 110 is input as 1.1).
-    public float baseknockback; //  attack's base knockback.
-    public float angleatteck; //    de angle van de atteck 
+    float weight = 68; //           de weight van gameobject
+    public float Scaling; //        the attack's knockback scaling (also known as knockback growth) divided by 100 (so a scaling of 110 is input as 1.1).
+    public float BaseKnockBack; //  attack's base knockback.
+    public float AngleAttack; //    de angle van de atteck  
 
     [SerializeField] Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -27,10 +27,11 @@ public class Jigglypuff : MonoBehaviour
         if (attacked || Input.GetKeyDown(KeyCode.T))
         {
             attacked = false;
-            Debug.Log((((percentage / 10) + ((percentage * damage) / 20) * (200 / (weight + 100) * 1.4) + 18) * scaling) + baseknockback);
-            force = (((percentage / 10) + ((percentage * damage) / 20) * (200 / (weight + 100) * 1.4) + 18) * scaling) + baseknockback;
-            rb.AddForce(((float)force) * Mathf.Cos(angleatteck), ((float)force) * Mathf.Sin(angleatteck), 0);
+            Debug.Log((((percentage / 10) + ((percentage * damage) / 20) * (200 / (weight + 100) * 1.4) + 18) * Scaling) + BaseKnockBack);
+            force = (((percentage / 10) + ((percentage * damage) / 20) * (200 / (weight + 100) * 1.4) + 18) * Scaling) + BaseKnockBack;
+            rb.AddForce(((float)force) * Mathf.Cos(AngleAttack), ((float)force) * Mathf.Sin(AngleAttack), 0);
             percentage += damage;
+            Debug.Log(damage);
         }
         if (transform.position.y <= -30 || transform.position.x <= -30 || transform.position.x >= 30 ||transform.position.y >= 30)
         {
@@ -39,5 +40,5 @@ public class Jigglypuff : MonoBehaviour
             rb.angularVelocity = Vector3.zero;
             percentage = 0;
         }
-    }
+    }   
 }
